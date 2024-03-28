a55293ef04e4e56038a93a8690c3f689c52d85b5
Keith A. Lewis <kal@kalx.net>
2024-03-28 16:43:32 -0400

    mean

--- mc_mean.cpp
+++ mc_mean.cpp
@@ -2,22 +2,45 @@
 #include "xll_mc.h"
 
 struct mc_mean {
-	double* state; // count and running mean
-	mc_mean(double* state)
-		: state(state)
+	double n, x; // count and running mean
+	mc_mean(double x, double n)
+		: n(n), x(x)
 	{ }
 	void init()
 	{
-		state[0] = 0;
-		state[1] = 0;
+		n = 0;
+		x = 0;
 	}
-	void next(double x)
+	void next(double x_)
 	{
-		state[0] += 1;
-		state[1] += (x - state[1])/state[0];
+		n += 1;
+		x += (x - x_)/n;
 	}
 	void done()
 	{ }
 };
 
 using namespace xll;
+
+static AddIn xai_mc_mean(
+	Function(XLL_LPOPER, "xll_mc_mean", "MC.MEAN")
+	.Arguments({
+		Arg(XLL_FP, "x", "is an array of random variables."),
+	})
+	.FunctionHelp("Return the mean of the random variables.")
+	.Category(CATEGORY)
+);
+LPOPER WINAPI xll_mc_mean(_FP12* px)
+{
+#pragma XLLEXPORT
+	static OPER o;
+
+	o = Excel(xlCoerce, Excel(xlfCaller));
+
+	if (state == NEXT) {
+		mc_mean m(px->array);
+		m.next();
+	}
+
+	return &o;
+}
